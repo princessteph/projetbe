5c067ba512a3b333c4fc368057f73f750a910c5e
princessteph <sanamahefa@gmail.com>
2026-07-20 09:21:32 +0300

    Create dbconnect.php

---
+++ pages/inc/dbconnect.php
@@ -0,0 +1,16 @@
+<?php
+function dbconnect(){
+    static $connect = null;
+
+    if ($connect === null) {
+        $connect = mysqli_connect('localhost', 'root', '', 'exam');
+
+        if (!$connect) {
+            die('Erreur de connexion à la base de données : ' . mysqli_connect_error());
+        }
+
+        mysqli_set_charset($connect, 'utf8mb4');
+    }
+
+    return $connect;
+}
